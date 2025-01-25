65a94dbbc6c79ec96fa4963bb29802e96be8c610
DESKTOP-TCJ9205\aa879 <aa8791@naver.com>
2025-01-25 23:12:30 +0900

    Freeboard_v0.9_Nam_commit

---
+++ Board/Freeboard/FreeBoard_v0.9_Nam.js
@@ -0,0 +1,23 @@
+window.addEventListener("load", function () {
+    console.log('게시판에 들어옴')
+})
+const search = document.querySelector("#search");
+search.addEventListener('click', filter)
+function filter() {
+    const optionS = document.querySelector('#viewfilter')
+    const optionT = document.querySelector('#fillvalue')
+    console.log(optionS.value)
+    console.log(optionT.value)
+}
+
+const write = document.querySelector('.write');
+write.addEventListener('click', writeprocess)
+function writeprocess(){
+    const writeprocess = document.querySelector('.boardcontents')
+    writeprocess.innerHTML += `<td class='num'> 번호 </td>
+<td class='writetitle'> 제목 </td>
+<td class='writeuser'> 작성자 </td>
+<td class='writedate'> 작성일 </td>
+<td class='viewcount'> 조회수 </td>
+</tr>`
+}
