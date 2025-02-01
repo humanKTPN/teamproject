b9d698064aa287ad2d259a2f9301b65c5c4586c9
DESKTOP-TCJ9205\aa879 <aa8791@naver.com>
2025-02-02 00:55:01 +0900

    idfindpage_v0.4_Nam_commit(파일명정리)

---
+++ idfindpage/idfindpage_Nam.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', message)
+function message() {
+    console.log("아이디 찾기 페이지 접근중")
+
+    document.querySelector('#result').addEventListener('click', idfinder)
+    function idfinder() {
+        let idfinder = document.querySelector('#result')
+        console.log('아이디찾기 버튼 클릭')
+
+        const name = document.querySelector('#name')
+        console.log(name.value)
+        const tel = document.querySelector('#tel')
+        console.log(tel.value)
+        const userfind = document.querySelector('.findres')
+
+        if ((name.value == "admin" && tel.value == '0415611122')) {
+            console.log('회원정보 찾음')
+            // 회원정보(id)값을 불러와야함.(현재 넣은 아이디는 임의값임)
+            userfind.textContent = '아이디는 adnin 입니다.'
+            userfind.style.color = 'black'
+        } else {
+            console.log('회원정보 없음')
+            userfind.textContent = '회원정보를 찾을 수 없습니다.'
+            userfind.style.color = 'red'
+        }
+
+        if (name.value == '') {
+            console.log('미입력된 항목이 있음')
+            userfind.style.color = '#f00';
+            userfind.textContent = '이름은 필수입니다!';
+        } else if (tel.value == '') {
+            console.log('미입력된 항목이 있음')
+            userfind.style.color = '#f00';
+            userfind.textContent = '전화번호는 필수입니다!';
+        }
+    }
+}
