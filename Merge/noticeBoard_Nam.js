35a31f4f79fa1833806bec36b0d4574ebe54ce0d
남기원 <aa8791@naver.com>
2025-02-03 12:06:20 +0900

    noticeboard_v0.2_Nam.commit(스타일수정[크기 및 테두리] 및 스왑화)

--- Merge/noticeBoard_Nam.js
+++ Merge/noticeBoard_Nam.js
@@ -1,4 +1,69 @@
-window.addEventListener("load", function () {
+window.addEventListener('load',init)
+
+                
+                
+function init(){
+
+    //메뉴 열었다 닫았다 하기<시작>
+    document.querySelector('#main-menu').addEventListener('click',function(){
+        
+        
+        if(document.querySelector('.mainmenu').classList.length == 1){
+            document.querySelector('.mainmenu').classList.add('hide')
+            document.querySelector('#main-menu').textContent = 'MENU 열기'
+        } else if (document.querySelector('.mainmenu').classList.length == 2){
+            document.querySelector('.mainmenu').classList.remove('hide')
+            document.querySelector('#main-menu').textContent = 'MENU 닫기'
+        }
+
+    }) //메뉴 열었다 닫았다 하기<끝>
+    // 탑 버튼 내용나오게 하기 <시작>
+    console.log(document.querySelector('.mainmenu').classList.length)// 클래스 개수
+    let menus = document.querySelectorAll('.menu')
+    for (menu of menus){//탑 메뉴탭에 클릭이벤트 주기
+        menu.addEventListener('click',function(e){
+
+            // console.log(e.target.textContent) // 어떤 메뉴를 눌렀는지 확인
+            // 클릭했을때 해당 페이지로 넘어가는 Java Script
+            if(e.target.textContent=='작업관리'){
+                // 작업관리 메인
+                window.location.href="#"
+            } else if (e.target.textContent=='생산관리'){
+                // 생산관리 메인 
+                window.location.href="#"
+            } else if (e.target.textContent=='생산리포팅'){
+                // 생산리포팅 메인 
+                window.location.href="#"
+            } else if (e.target.textContent=='자재관리'){
+                // 자재관리 메인 
+                window.location.href="#"
+            } else if (e.target.textContent=='품질관리'){
+                // 품질관리 메인 
+                window.location.href="#"
+            } 
+        })
+    }// 탑 버튼 내용나오게 하기  <끝>
+
+
+    // 메뉴 버튼 눌렀을때 해당 내용 나오게 하기<시작>
+    let buttons = document.querySelectorAll('.menuBut')
+    for(button of buttons){
+        button.addEventListener('click',function(e){
+            if(e.target.textContent=='대시보드'){
+                 // 대시보드 메인
+                window.location.href="#"
+            } else if (e.target.textContent=='게시판'){
+                 // 게시판 메인
+                window.location.href="#"
+            } else if (e.target.textContent=='공지사항'){
+                 // 공지사항 메인
+                window.location.href="#"
+            }  else if (e.target.textContent=='기준관리'){
+                 // 기준관리 메인
+                window.location.href="#"
+            }
+        })
+    }// 메뉴 버튼 눌렀을때 해당 내용 나오게 하기<끝>
     console.log('게시판에 들어옴')
 
     const search = document.querySelector("#search");
@@ -17,17 +82,6 @@ window.addEventListener("load", function () {
     function writeprocess() {
         window.location.href = "writeprocess_nam.html"
         console.log("글쓰기 페이지 진입")
-
-
-
-        //         const writeprocess = document.querySelector('.boardcontents')
-        //         writeprocess.innerHTML += `<tr class='boardcontents' style="height: 30px; border: 1px solid black;">
-        //     <td class='num'> 번호 </td>
-        // <td class='writetitle'> 제목 </td>
-        // <td class='writeuser'> 작성자 </td>
-        // <td class='writedate'> 작성일 </td>
-        // <td class='viewcount'> 조회수 </td>
-        // </tr>`
     }
     // 페이지표시부분 미구현
     const page1 = document.querySelector('.pnum1')
@@ -61,4 +115,4 @@ window.addEventListener("load", function () {
         console.log('5페이지 진입')
     })
 
-})
+}
