0b22b56ec6eda76aab58744aee37f6d9ed21110e
곽우림 <dlwpekfms@gmail.com>
2025-04-17 16:04:06 +0900

    css, js 추가 커밋

--- proj_spring_ktpn/src/main/webapp/resources/js/bom_main_kwak.js
+++ proj_spring_ktpn/src/main/webapp/resources/js/bom_main_kwak.js
@@ -4,10 +4,9 @@ window.addEventListener('load',init)
                 
 function init(){
 
+    const tbnms = document.querySelectorAll('.tab-name')
     
-    tbnms = document.querySelectorAll('.tab-name')
-
-    for(tbnm of tbnms){
+	for(tbnm of tbnms){
         tbnm.addEventListener('click',function(e){
             console.log(e.target.textContent)
             if(e.target.textContent == '기준관리'){
@@ -15,23 +14,12 @@ function init(){
             } else if(e.target.textContent == 'BOM'){
                 window.location.href = '/ktpn/bm'
             } else if(e.target.textContent == '공정'){
-                window.location.href = '/ktpn/md'
+                window.location.href = 'processDesc_main.jsp'
             }
         })
     }
     
-     document.querySelector('.add').addEventListener('click',function(){
-        // alert("추가되었습니다.")
-        window.location.href="/ktpn/mdAdd"
+    document.querySelector('.add').addEventListener('click',function(){
+    	window.location.href="/ktpn/bmAddDetail"
     })
-
-
 }
-
-
-    
-    
-
-
-
-// }
--- proj_spring_ktpn/src/main/webapp/resources/js/masterdataManage_add_kwak.js
+++ proj_spring_ktpn/src/main/webapp/resources/js/masterdataManage_add_kwak.js
@@ -7,7 +7,7 @@ function init(){
    
     
     document.querySelector('.add').addEventListener('click',function(){
-        alert("추가되었습니다.")
+        //alert("추가되었습니다.")
         window.location.href="/ktpn/md"
     })
      
